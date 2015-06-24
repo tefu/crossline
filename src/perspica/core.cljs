a96778f0bc613dd9b76bbfeb3462ecc7e9e98eed
Trevor Hullinger <hakoniw@gmail.com>
2015-06-24 06:10:58 -0600

    Add starting CSS and React elements

--- src/perspica/core.cljs
+++ src/perspica/core.cljs
@@ -1,27 +1,56 @@
 (ns ^:figwheel-always perspica.core
     (:require[om.core :as om :include-macros true]
-              [om.dom :as dom :include-macros true]))
+             [om.dom :as d :include-macros true]))
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
+(def app-state (atom {:text "Hello world!"
+                      :sketchpad-width 500
+                      :sketchpad-height 500
+                      :brush-limit 5}))
 
-;; define your app data so that it doesn't get over-written on reload
+(defn make-brush [data owner]
+  (apply
+   (partial d/table #js {:id "brush-size"})
+   (for [x (range (:brush-limit data))]
+     (apply
+      (partial d/tr nil)
+      (for [y (range (:brush-limit data))]
+        (d/td nil "a"))))))
 
-(defonce app-state (atom {:text "Hello world!"}))
+
+(defn main-app [data owner]
+  (reify om/IRender
+    (render [_]
+      (d/div
+       nil
+       (d/div
+        #js {:id "left-options-bar"}
+        (d/div #js {:id "texture-palette"} "texture-palette")
+        (make-brush data owner)
+        (d/canvas #js {:id "perspective-grid"} "perspective-grid")
+        (d/div #js {:id "perspective-selection"} "perspective-selection"
+               (d/li nil "1 pt")
+               (d/li nil "2 pt")
+               (d/li nil "3 pt")
+               (d/li nil "4 pt")
+               (d/li nil "5 pt")))
+       (d/div
+        #js {:id "sketchpad-frame"}
+        "sketchpad-frame"
+        (d/canvas #js {:id "sketch-pad"
+                       :width (:sketchpad-width data)
+                       :height (:sketchpad-height data)})
+        (d/div #js {:id "time-machine"}))))))
 
 (om/root
-  (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text data)))))
-  app-state
-  {:target (. js/document (getElementById "app"))})
+ main-app
+ app-state
+ {:target (. js/document (getElementById "app"))})
 
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-) 
-
+  )
